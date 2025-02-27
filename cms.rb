3294c5adaf7ba08f50c15e84455919137bed114b
Joshua Michael Hall <sejoshuahall@gmail.com>
2025-02-27 10:34:47 -0600

    My solution lesson 2

--- cms.rb
+++ cms.rb
@@ -7,6 +7,7 @@ configure do
   enable :sessions
   set :session_secret, SecureRandom.hex(32)
   set :erb, :escape_html => true
+  set :views, File.join(settings.public_folder, 'views')
 end
 
 helpers do
@@ -18,5 +19,5 @@ before do
 end
 
 get "/" do
-  redirect "/lists"
+  erb :home, layout: :layout
 end
