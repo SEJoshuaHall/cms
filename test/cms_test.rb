7053d7a4089d670c13865d54c37b931e222f5233
Joshua Michael Hall <sejoshuahall@gmail.com>
2025-02-28 14:37:30 -0600

    Minor refactoring after viewing LS solution Lesson 5

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -5,7 +5,7 @@ require "rack/test"
 
 require_relative "../cms"
 
-class AppTest < Minitest::Test
+class CMSTest < Minitest::Test
   include Rack::Test::Methods
 
   def app
@@ -14,6 +14,7 @@ class AppTest < Minitest::Test
 
   def test_index
     get "/"
+
     assert_equal 200, last_response.status
     assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
     assert_includes(last_response.body, "about.txt")
@@ -23,6 +24,7 @@ class AppTest < Minitest::Test
 
   def test_file_display
     get "/about.txt"
+
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
     assert_includes(last_response.body, "ipsum")
