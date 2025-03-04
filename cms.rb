b7082a7b021267403616903e0acbbd3d53c6e7b0
Joshua Michael Hall <sejoshuahall@gmail.com>
2025-03-04 08:08:54 -0600

    passes all tests

--- cms.rb
+++ cms.rb
@@ -100,7 +100,7 @@ post "/create" do
   end
 end
 
-post "/:filename/destroy" do
+post "/:filename/delete" do
   file_path = File.join(data_path, params[:filename]) 
   File.delete(file_path)
   session[:message] = "#{params[:filename]} has been deleted." 
